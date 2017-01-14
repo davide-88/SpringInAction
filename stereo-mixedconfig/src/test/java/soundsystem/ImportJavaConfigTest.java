6b9129d00e271cfbe86fd9a85bcba8222d89f48a
Davide Mantelli <davide.mantelli88@gmail.com>
2017-01-14 16:29:16 +0100

    Bug in assertEqual fixed: "\n" works only in Unix-like systems.

--- stereo-mixedconfig/src/test/java/soundsystem/ImportJavaConfigTest.java
+++ stereo-mixedconfig/src/test/java/soundsystem/ImportJavaConfigTest.java
@@ -4,7 +4,7 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.Rule;
 import org.junit.Test;
-import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.contrib.java.lang.system.SystemOutRule;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ContextConfiguration;
@@ -14,19 +14,19 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 @ContextConfiguration("classpath:cdplayer-config.xml")
 public class ImportJavaConfigTest {
 
-  @Rule
-  public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+	@Rule
+	public final SystemOutRule log = new SystemOutRule();
 
-  @Autowired
-  private MediaPlayer player;
+	@Autowired
+	private MediaPlayer player;
 
-
-  @Test
-  public void play() {
-    player.play();
-    assertEquals(
-        "Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles\n",
-        log.getLog());
-  }
+	@Test
+	public void play() {
+		log.enableLog();
+		player.play();
+		assertEquals(String.format("Playing Sgt. Pepper's Lonely Hearts Club Band by The Beatles%n"),
+				log.getLog());
+		log.clearLog();
+	}
 
 }
